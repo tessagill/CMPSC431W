7823e96666e1117fd541b4c4766d5570ce697714
Jared Cole <69369570+JaredCCole@users.noreply.github.com>
2023-11-30 20:55:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from commands import *
 def getUser():
     start = getValidInput('\nAre you a new or existing user? ', options=['new', 'existing'])
 
-    if start.lower() == 'new':
+    if start == 'new':
         clear()
         print('Welcome!')
         fname = input('Enter your first name: ')
@@ -18,27 +18,63 @@ def getUser():
         incomeType = getValidInput('\nIs your income hourly or salaried?\n', options=['hourly', 'salaried'])
         
         if incomeType == 'hourly':
-            hourly_wage = getValidInput('\nEnter your hourly pay: ', number=True)
-            hours_worked = getValidInput('\nEnter your weekly hours worked: ', number=True)
+            hourly_wage = getValidInput('\nEnter your hourly pay: ', decimal=True)
+            hours_worked = getValidInput('\nEnter your weekly hours worked: ', decimal=True)
             newUser = HourlyPaidUser(newUser.fname, newUser.lname, newUser.email, hourly_wage, hours_worked)
         elif incomeType == 'salaried':
-            salary = getValidInput('\nEnter your annual pay: ', number=True)
+            salary = getValidInput('\nEnter your annual pay: ', decimal=True)
             newUser = HourlyPaidUser(newUser.fname, newUser.lname, newUser.email, salary)
         
         return newUser
         
 
-    elif start.lower() == 'existing':
+    elif start == 'existing':
         userID = input('Enter your UserID: ')
         user = User.searchUserID(userID)
         return user
 
 
 def main():
-    getUser()
+    user = getUser() # retrieves new or existing user
     while True:
+        clear()
         listCommands()
-        pass
+        command = input().lower()
+        print()
+
+        # ALL COMMANDS
+        if command == "create new debt":
+            createNewDebt(user)
+        elif command == "create recurring payment":
+            createNewRecurring(user)
+        elif command == "create upcoming payment":
+            createNewUpcomingPayment(user)
+        elif command == "create transaction":
+            createNewTransaction(user)
+        elif command == "create new budget":
+            print("Handling CREATE new budget")
+        elif command == "update income":
+            user = updateIncome(user) # returns the updated user object with new income
+        elif command == "update debt":
+            updateDebt(user)
+        elif command == "update investments":
+            print("Handling UPDATE investments")
+        elif command == "update recurring":
+            print("Handling UPDATE recurring")
+        elif command == "update planned payments":
+            print("Handling UPDATE planned payments")
+        elif command == "view debt payoff timeline":
+            print("Handling VIEW debt payoff timeline")
+        elif command == "view predicted month's spending":
+            print("Handling VIEW predicted month's spending")
+        elif command == "view expenses by category":
+            print("Handling VIEW expenses by category")
+        elif command == "view budget":
+            print("Handling VIEW budget")
+        else:
+            print("Unknown command")
+
+
 
 
 
