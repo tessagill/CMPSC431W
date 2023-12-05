dda041bb43ca4b0003840ea5807eb5f1dea2fba2
Jared Cole <69369570+JaredCCole@users.noreply.github.com>
2023-12-05 06:17:17 -0500

    Delete Debt.py

--- Debt.py
+++
@@ -1,7 +0,0 @@
-class Debt:
-    def __init__(self, userID, debt_amount, debt_type, interest_rate, min_payment):
-        self.userID = userID
-        self.debt_amount = debt_amount
-        self.debt_type = debt_type
-        self.interest_rate = interest_rate
-        self.min_payment = min_payment
