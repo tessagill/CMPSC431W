6f99d42a449456fa278b561555112beb47440853
Jared Cole <69369570+JaredCCole@users.noreply.github.com>
2023-12-05 06:17:59 -0500

    Delete Transactions.py

--- Transactions.py
+++
@@ -1,8 +0,0 @@
-class Transactions:
-    def __init__(self, userID, tranID, amount, date, type, name):
-        self.userID = userID
-        self.tranID = tranID
-        self.amount = amount
-        self.date = date
-        self.type = type
-        self.name = name
