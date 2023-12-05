3323920ccdeb553ca2d3a5272949ec306f899326
Jared Cole <69369570+JaredCCole@users.noreply.github.com>
2023-12-05 06:17:27 -0500

    Delete Budget.py

--- Budget.py
+++
@@ -1,6 +0,0 @@
-class Budget:
-    def __init__(self, userID, budget_type, allocated_amount, creation_date):
-        self.userID = userID
-        self.budget_type = budget_type
-        self.allocated_amount = allocated_amount
-        self.creation_date = creation_date
