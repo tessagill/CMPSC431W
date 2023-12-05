8b333830da366fb557ae0d794ed793ea856a68bb
Jared Cole <69369570+JaredCCole@users.noreply.github.com>
2023-12-05 06:17:47 -0500

    Delete PlannedPayments.py

--- PlannedPayments.py
+++
@@ -1,7 +0,0 @@
-class PlannedPayments:
-    def __init__(self, userID, date_by, title, amount, recurring):
-        self.userID = userID
-        self.title = title
-        self.date_by = date_by
-        self.amount = amount
-        self.recurring = recurring
