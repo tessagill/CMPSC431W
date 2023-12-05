322db5ed5121a84a89c3929c7bb5b41633bd40f8
Jared Cole <69369570+JaredCCole@users.noreply.github.com>
2023-12-05 06:17:37 -0500

    Delete Investments.py

--- Investments.py
+++
@@ -1,5 +0,0 @@
-class Investments:
-    def __init__(self, userID, portfolio_total, risk_percent):
-        self.userID = userID
-        self.portfolio_total = portfolio_total
-        self.risk_percent = risk_percent
